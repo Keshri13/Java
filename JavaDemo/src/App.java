a89bd8fa71f3c8803ad0c359fc375c7a69108404
Keshri13 <keshri1316@gmail.com>
2023-05-04 12:56:09 +0530

    Static Implementation

--- JavaDemo/src/App.java
+++ JavaDemo/src/App.java
@@ -1,9 +1,31 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws ClassNotFoundException {
 
-        System.out.println("Hello, I am Keshu, And it seems I am not good as I was once");
+        //System.out.println("Hello, I am Keshu, And it seems I am not good as I was once");
         // DataTypes dataTypes = new DataTypes();
         // DataTypes.printX();
+
+        //implementBikes();
         
+        
+    }
+
+    static void implementBikes() {
+
+        //Bike bike = new Bike();
+        
+        //For Loading the Bike class where the static blocks are called first when the class is loaded for the first time
+        //Class.forName("Bike");
+        
+        // System.out.println(Bike.fuelUsed);
+        // System.out.println(Bike.noOfWheels);
+
+        Bike heroBike = new Bike();
+        heroBike.display();
+
+        Bike hondaBike = new Bike(250, 15, 180, "Honda");
+        hondaBike.display();
+        System.out.println("------------------------------------------");
+
     }
 }
---
+++ JavaDemo/src/Bike.java
@@ -0,0 +1,44 @@
+public class Bike {
+    
+    int engineCapacity;
+    int tankCapacity;
+    int topSpeed;
+    String manufacturingBrand;
+
+    static int noOfWheels = 2;
+    static String fuelUsed = "Petrol";
+    
+    static {
+        noOfWheels = 2;
+        fuelUsed = "Petrol";
+        System.out.println("In Static Block");
+    }
+
+    Bike() {
+        engineCapacity = 160;
+        tankCapacity = 16;
+        topSpeed = 123;
+        manufacturingBrand = "Hero Honda";
+    }
+
+    Bike(int engineCapacity,int tankCapacity,int topSpeed,String manufacturingBrand) {
+        this.engineCapacity =engineCapacity;
+        this.tankCapacity=tankCapacity;
+        this.topSpeed=topSpeed;
+        this.manufacturingBrand = manufacturingBrand;
+    }
+
+    public void display() {
+
+        System.out.println("------------------------------------------");
+        System.out.println("Engine Capacity : " + engineCapacity +" cc");
+        System.out.println("Tank Capacity : " + tankCapacity+" Ltr");
+        System.out.println("Top Speed : " + topSpeed+" Kmph");
+        System.out.println("Manufacturing Brand : " + manufacturingBrand);
+        System.out.println("Number of Wheels used : " + noOfWheels);
+        System.out.println("Fuel used : " + fuelUsed);
+        //System.out.println("------------------------------------------");
+
+    }
+
+}
