afe13a72efc8166e3055c1cc95ad8766f52d4ed1
Keshri13 <keshri1316@gmail.com>
2023-06-05 22:57:19 +0530

    Bike is mine

--- JavaDemo/src/Bike.java
+++ JavaDemo/src/Bike.java
@@ -1,5 +1,5 @@
 public class Bike {
-    
+
     int engineCapacity;
     int tankCapacity;
     int topSpeed;
@@ -7,7 +7,7 @@ public class Bike {
 
     static int noOfWheels = 2;
     static String fuelUsed = "Petrol";
-    
+
     static {
         noOfWheels = 2;
         fuelUsed = "Petrol";
@@ -21,23 +21,23 @@ public class Bike {
         manufacturingBrand = "Hero Honda";
     }
 
-    Bike(int engineCapacity,int tankCapacity,int topSpeed,String manufacturingBrand) {
-        this.engineCapacity =engineCapacity;
-        this.tankCapacity=tankCapacity;
-        this.topSpeed=topSpeed;
+    Bike(int engineCapacity, int tankCapacity, int topSpeed, String manufacturingBrand) {
+        this.engineCapacity = engineCapacity;
+        this.tankCapacity = tankCapacity;
+        this.topSpeed = topSpeed;
         this.manufacturingBrand = manufacturingBrand;
     }
 
     public void display() {
 
         System.out.println("------------------------------------------");
-        System.out.println("Engine Capacity : " + engineCapacity +" cc");
-        System.out.println("Tank Capacity : " + tankCapacity+" Ltr");
-        System.out.println("Top Speed : " + topSpeed+" Kmph");
+        System.out.println("Engine Capacity : " + engineCapacity + " cc");
+        System.out.println("Tank Capacity : " + tankCapacity + " Ltr");
+        System.out.println("Top Speed : " + topSpeed + " Kmph");
         System.out.println("Manufacturing Brand : " + manufacturingBrand);
         System.out.println("Number of Wheels used : " + noOfWheels);
         System.out.println("Fuel used : " + fuelUsed);
-        //System.out.println("------------------------------------------");
+        // System.out.println("------------------------------------------");
 
     }
 
